11bd0836f9a965a2beb238f84c2bbb771462cb79
Joe-abdo <49865055+Joe-abdo@users.noreply.github.com>
2019-10-21 22:13:37 +0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -7,4 +7,5 @@ $conn       = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+mysqli_set_charset($conn,"utf8mb4");
 ?>
