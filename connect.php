8ff39b5d789b118eefde2c7e34ab106080344389
Joe-abdo <49865055+Joe-abdo@users.noreply.github.com>
2020-03-02 20:11:08 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "localhost";
-$username   = "root";
-$password   = "";
+$servername = "██████████████████";
+$username   = "████████████";
+$password   = "█████████████████";
 $dbname     = "id11552830_uploads";
 $conn       = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
